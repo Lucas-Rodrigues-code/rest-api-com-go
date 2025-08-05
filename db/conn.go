80e6bfb970db32669fa77f5bcdac3c4d48df97e7
lucas <lucas.rodrigues@neocredit.com.br>
2025-08-05 20:41:52 -0300

    feat: add database connection functionality for PostgreSQL

---
+++ db/conn.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = 1234
+	dbname   = "db_go"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%d dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connect to " + dbname)
+
+	return db, nil
+}
