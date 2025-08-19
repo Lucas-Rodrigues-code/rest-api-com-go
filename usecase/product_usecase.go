cd782a3233086902b159e4943b1c3e851fa340e2
lucas <lucas.rodrigues@neocredit.com.br>
2025-08-18 22:19:45 -0300

    feat: implement CreateProduct method in ProductUseCase and add corresponding controller logic

--- usecase/product_usecase.go
+++ usecase/product_usecase.go
@@ -18,3 +18,13 @@ func NewProductUseCase(repository repository.ProductRepository) ProductUseCase {
 func (pu *ProductUseCase) GetProducts() ([]model.Product, error) {
 	return pu.repository.GetProducts()
 }
+
+func (pu *ProductUseCase) CreateProduct(product model.Product) (model.Product, error) {
+	productId, err := pu.repository.CreateProduct(product)
+
+	if err != nil {
+		return model.Product{}, err
+	}
+	product.Id = productId
+	return product, nil
+}
